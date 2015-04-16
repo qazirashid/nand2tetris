b203ade44973a775db83979ac0ca39e99b7edb57
qazi <qazi@example.com>
2015-04-16 15:54:06 +0100

    Skeleton of CodeWriter is ready

--- projects/07/cpp/parser.cpp
+++ projects/07/cpp/parser.cpp
@@ -14,7 +14,7 @@ class vmparser{
 
 private:
   std::fstream infile;
-  std::string current_command, arg1, arg2;
+  std::string current_command;
   vm_command_type command_type;
   int line_number;
   std::vector<std::string> arith_commands;
@@ -24,10 +24,10 @@ private:
 public:
   vmparser(std::string filename){
     
-    std::cout << "Constructor called with file name" << filename << std::endl;
+    std::cout << " VM parser constructor called with file name" << filename << std::endl;
     infile.open(filename.c_str(), std::ios::in); // string.c_str is used to convert std:string to const char *.
     if(!infile)
-      std::cout << "Error Opening Input file\n";
+      std::cout << "VM PARSER: Error Opening Input file\n";
     line_number = 0;
     const std::string ac[] = {"add","sub","neg","eq","gt", "lt", "and", "or", "not"};
     arith_commands.insert(arith_commands.begin(), ac, ac+9); // populate the vector
@@ -53,7 +53,7 @@ public:
       //	i++;
       //} //end of for loop
     } //end of if
-    std::cout<<std::endl;
+    //std::cout<<std::endl;
   }//end of advance
   
   void getCurrentString( std::string& line){
@@ -107,7 +107,7 @@ public:
       }
       else{
 	start = pos;
-	while((s[pos] != ' ') && (s[pos] != '\r') && (s[pos] != '\n')){
+	while((pos < maxlen)&&(s[pos] != ' ') && (s[pos] != '\r') && (s[pos] != '\n')){
 	  len++;
 	  pos++; 
 	}//end of inner while counting the non-delimited characters in 
@@ -117,31 +117,112 @@ public:
       }
     }//end of outer while    
   }//end of getTokens();
-
+  
+  std::string arg1(){
+    if(tokens.size() < 1)
+      return("");
+    return(tokens[0]);
+  }
+  std::string arg2(){
+    if(tokens.size()<2)
+      return("");
+    return(tokens[1]);
+  }
+  std::string arg3(){
+    if(tokens.size() < 3)
+      return("");
+    return(tokens[2]);
+  }
   ~vmparser(){
     infile.close();
   }
 
 
 
-};
+}; //end of class vmparser
+
+
+class codewriter{
+private:
+  std::fstream outfile;
+  std::string newfilename;
+
+  //std::vector<std::string> arith_commands;
+  //std::vector<std::string> mem_segments;
+  //std::vector<std::string> tokens;
+public:  
+  codewriter(std::string filename){
+    
+    std::cout << "Code Writer constructor called with file name" << filename << std::endl;
+    outfile.open(filename.c_str(), std::ios::out);
+    if(!outfile)
+      std::cout << "CODE WRITER: Error Opening Output file\n";
+  }
+
+  
+  void setFileName(std::string str) {
+    newfilename = str;
+  }
+  int writeArithmetic(std::string str){
+    outfile << "Wrote Arithmetic Command:" << str << std::endl;
+    return(1);
+  }
+  int writePushPop(vm_command_type ct, std::string segment, int index){
+    switch(ct){
+    case C_PUSH:
+      outfile << "Wrote Push Command for Segment " << segment << " at index " << index<< std::endl;
+      return(1);
+    case C_POP:
+      outfile << "Wrote Push Command for Segment " << segment << " at index " << index<< std::endl;
+      return(1);
+    default:
+      std::cout << "The command is not Push or Pop\n";
+      return(0);
+    }//end of switch
+
+  }//end of writePushPop
+ 
+
+  ~codewriter(){
+    outfile.close(); //release resources
+  }  
+
+
+}; // End of class codewriter
 
 
 int main(void){
-  std::cout << "Hello World \n";
+  //std::cout << "Hello World \n";
   std::string in = "test.vm";
+  std::string out ="testout.asm";
   std::string  line;
   vm_command_type command_type;
 
   vmparser parser(in);
+  codewriter writer(out);
   while(parser.hasMoreCommands()){
     parser.advance();
     parser.getCurrentString(line);
-    std::cout<< "LINE=" << parser.getLineNumber() << ":" << line << std::endl;
+    std::cout<< "LINE=" << parser.getLineNumber() << ":" << line <<std::endl;
     command_type = parser.commandType();
-    std::cout << enumlist[command_type] << std::endl;
-
+    std::cout << "COMMAND TYPE =" << enumlist[command_type] << "    ARG1=" << parser.arg1() << "    ARG2=" << parser.arg2()<< std::endl;
+    
+    switch(command_type){
+    case C_ARITHMETIC:
+      writer.writeArithmetic(parser.arg1());
+      break;
+    case C_PUSH:
+      writer.writePushPop(C_PUSH, parser.arg2(), 2); //parser gives us string for index. we will need to convert it to integer.
+      break;
+    case C_POP:
+      writer.writePushPop(C_POP, parser.arg2(), 3);
+      break;
+    case C_INVALID:
+      std::cout << "ERROR: This command is not in the VM specification: LINE NO: " << parser.getLineNumber()<< std::endl;
+      break;
+    default:
+      std::cout<<"This command is not implemented." << std::endl;
+      break;
+    }
   }
-  
-
 }
